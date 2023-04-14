01ae5dcf4248fc9af2ae4029d0c78058ca12602d
Vladimir <vovan0503@mail.ru>
2023-04-14 21:52:51 +0300

    Commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
-import { Layout, Button, Typography } from 'antd';
+import { useSelector } from 'react-redux';
+import { Layout, Typography } from 'antd';
+import { ShoppingCartOutlined } from '@ant-design/icons';
+
+import './Header.scss';
 
 const Header = () => {
+
+    const { cartItems } = useSelector(state => state.cart)
 
     const { Title } = Typography;
     const { Header } = Layout;
 
     return (
-        <Layout className="layout">
+        <Layout>
             <Header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
                 <Title style={{ color: 'white', margin: '0px' }}>Shop</Title>
-                {/* <Button onClick={exit}>Выйти</Button> */}
+                <div className='cartLink'>
+                    <ShoppingCartOutlined style={{ fontSize: '50px', color: 'white' }} />
+                    <div className="cartLink__count">
+                        <p>{cartItems.length}</p>
+                    </div>
+                </div>
             </Header>
         </Layout>
     );
