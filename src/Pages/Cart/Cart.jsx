9e99b9a13f6415ce063a4ead1cb5734253dbd187
unknown <Vladimir>
2023-04-15 03:10:39 +0300

    Commit

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import CartList from '../../features/Cart/CartList';
+import { useSelector, useDispatch } from 'react-redux';
+
+import './Cart.scss';
 
 const Cart = () => {
+
+    const { cartItems, totalPrice } = useSelector(state => state.cart);
+
     return (
-        <div className='Cart'>
-            Корзина
+        <div className='cart'>
+            <CartList items={cartItems} totalPrice={totalPrice} />
         </div>
     );
 };
