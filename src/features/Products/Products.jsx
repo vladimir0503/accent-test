2d9d3ee481a9188f0a36c936b6689088059ffae8
Vladimir <vovan0503@mail.ru>
2023-04-16 11:25:45 +0300

    Commit

--- src/features/Products/Products.jsx
+++ src/features/Products/Products.jsx
@@ -1,30 +1,32 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Pagination } from 'antd';
+import { Pagination, List } from 'antd';
 import Title from '../../components/Title/Title';
 import Product from '../Products/Product/Product';
 
 import './Products.scss';
 
 const Products = () => {
-  // const [currentPage, setCurrentPage] = React.useState(1);
 
   const { productsList } = useSelector(state => state.products);
-
-  // const handlePageChange = page => {
-  //   console.log(page);
-  //   setCurrentPage(page);
-  // };
-
-  // console.log(productsList);
 
   return (
     <div className='products'>
       {!!productsList.length
-        ? productsList.map((product, i) => <Product key={i} product={product}/>)
+        ? <List
+          grid={{ gutter: 16, column: 3 }}
+          pagination={{
+            position: 'bottom',
+            align: 'start',
+            pageSize: 6
+          }}
+          dataSource={productsList}
+          renderItem={(item, index) => (
+            <Product key={index} product={item} />
+          )}
+        />
         : <Title level={2}>Выберите бренд</Title>
       }
-      {/* {productsList.length > 6 && <Pagination pageSize={6} current={currentPage} total={productsList.length} onChange={handlePageChange} />} */}
     </div>
   );
 };
